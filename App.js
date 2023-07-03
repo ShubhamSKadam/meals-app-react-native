2c26925f2a75832514d2e2f46c0d3a5d48a97f99
subbarayudu.nidumolu <subbarayudu.nidumolu@godigit.com>
2023-07-03 10:55:15 +0530

    Implementing navigation between two screens

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
-  );
+  ); 
+
 };
 
 export default App;
